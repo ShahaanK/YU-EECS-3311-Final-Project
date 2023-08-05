2896caf68c6dd5d83c8a115312cdb4bef8fc2370
utkina-ekaterina <utkinaek@my.yorku.ca>
2023-08-05 16:54:07 -0400

    Revert "Added two lines in App and fixed errors"
    This reverts commit 593f0dd5dd2a4ac2e5b877646b16d2dc7155608a.

--- project/src/main/java/ca/yorku/eecs/App.java
+++ project/src/main/java/ca/yorku/eecs/App.java
@@ -12,11 +12,6 @@ public class App
         HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", PORT), 0);
         // TODO: two lines of code are expected to be added here
         // please refer to the HTML server example 
-        
-        //two lines of code from the BasicHttpServerExample (Ekaterina)
-        Utils utils = new Utils();
-	    server.createContext("/api/addTwoNumbers", utils::handle);
-        
         server.start();
         System.out.printf("Server started on port %d...\n", PORT);
     }
