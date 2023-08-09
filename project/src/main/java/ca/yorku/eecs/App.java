c8e1466721a42786819f7e691ab75b047c5c5fe4
Shahaan Khan <shahaanzia1@gmail.com>
2023-08-09 13:05:06 -0400

    Updated files to match robot
    Made adjustments to ports to enable matching to neo4j ports

--- project/src/main/java/ca/yorku/eecs/App.java
+++ project/src/main/java/ca/yorku/eecs/App.java
@@ -6,7 +6,7 @@ import com.sun.net.httpserver.HttpServer;
 
 public class App 
 {
-    static int PORT = 8080;
+    static int PORT = 7474;
     public static void main(String[] args) throws IOException
     {
         HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", PORT), 0);
